a56e593f9f4b88f4f99b410c1e58ffc01b76c88c
twonglikescurrie <tavonga.majada@kingsway.school.nz>
2026-05-22 14:33:41 +1200

    Input/Output basics

---
+++ P_03_input_output.py
@@ -0,0 +1,22 @@
+# Ask user for their name
+username = input("What is your name? ")
+
+# Ask the user for their favourite number (integer)
+ fav_num = int(input("What is your favourite number? "))
+
+# Double, half and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num ** 2
+
+# Greet the user
+print()
+print(f\n"Hello {username}, your favourite number is {fav_num}.")
+
+# Output the results of doubling, halving and squaring their favourite integer
+print()
+print(f"Double {fav_num} is {double}.")
+print(f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day.")
