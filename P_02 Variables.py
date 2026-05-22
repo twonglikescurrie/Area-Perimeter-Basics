3fef343577441c8f6c7ee0413c29daa1232e8656
twonglikescurrie <tavonga.majada@kingsway.school.nz>
2026-05-22 14:23:28 +1200

    Variables

---
+++ P_02 Variables.py
@@ -0,0 +1,15 @@
+# Code to learn about variables
+
+# Variables can be a string (Note the speech marks)
+greeting = "hello world"
+
+#Variables can be a number
+fav_num = 7
+print(fav_num)
+
+num_string = "5"
+print(fav_num * 2)
+print(num_string * 5)
+
+#constants are uppercase
+PI = 3.141592654
