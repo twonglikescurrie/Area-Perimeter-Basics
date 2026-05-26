bc5dca1a74565b4ca4aee3bbd25003c941ec4da0
twonglikescurrie <tavonga.majada@kingsway.school.nz>
2026-05-27 10:21:52 +1200

    Area perimeter calculator

---
+++ P_06_area_perimeter_calculator.py
@@ -0,0 +1,14 @@
+# Ask the user for the width and height
+# (assume they put in valid data)
+width = float(input("Width: "))
+height = float(input("height: "))
+
+# Calculate the area / perimeter
+area = width * height
+perimeter = 2 * (width + height)
+
+
+# Output the area and perimeter
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"Area: {area} square units")
