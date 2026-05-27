99c18d0943c6723e2e154e1dba6d9baff0492c64
twonglikescurrie <tavonga.majada@kingsway.school.nz>
2026-05-27 14:27:38 +1200

    Calculates fence cost (Meters)

---
+++ Working_fence_cost_calculator.py
@@ -0,0 +1,46 @@
+# Function goes here
+def num_check(question):
+
+    error = "Please enter a number greater than zero\n"
+
+    while True:
+
+        try:
+            response = float(input(question))
+
+            if response > 0:
+                return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine starts here...
+
+keep_going = ""
+
+while keep_going == "":
+
+    # Get width and height
+    width = num_check("Width: ")
+    height = num_check("Height: ")
+    cost = num_check("Cost per meter: $")
+
+    # Calculate perimeter and price for the fence
+    perimeter = 2 * (width + height)
+
+    price = perimeter * cost
+
+    # Display output
+    print()
+    print(f"Perimeter: {perimeter} units")
+    print(f"Price: ${price:.2f}")
+
+    # Ask user if they want to keep going
+    keep_going = input("\nPress <enter> to keep going or any key to quit: ")
+
+print()
+print("Program ended")
